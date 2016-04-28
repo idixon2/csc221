66b4133eb09e0aa0fc022ea2e1b668377ab8eebe
idixon2 <ieshia3@juno.com>
2016-04-28 17:10:56 +0000

    adding test_final_project.unittest.py

---
+++ project/test_final_project.unittest.py
@@ -0,0 +1,19 @@
+import unittest
+from final_project import (
+    is_leap_year
+)
+
+class TestFinal(unittest.TestCase):
+    def test_is_leap_year(self):
+        self.assertTrue(is_leap_year('1996'))
+        self.assertFalse(is_leap_year('1990'))
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
